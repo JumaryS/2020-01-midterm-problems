97532939283b1de50070e2542bc671b1c52978f1
jumi sanchez <jumary.sanchez@codeimmersives.com>
2020-03-03 12:02:11 -0500

    m

--- main.js
+++ main.js
@@ -2,7 +2,54 @@
  * YOUR CODE HERE *
  ******************/
 
+function fizzBuzzSingleNumber(str='', num){
 
+  
+
+  for(let i=0; i< str.length;i++){
+
+    if(num % 3 === 0 && num % 5 === 0){
+      return( "FizzBuzz") 
+    }else if (num % 3 === 0){
+      return( "Fizz")
+    }else if(num % 5 === 0){
+      return("Buzz")
+    } else{
+      
+    }
+      
+    }return str
+
+    
+    }
+  
+
+    function getMiddleLetters(word){
+    let middle = ''
+    for (let i=0; i<word.length; i++){
+      return Math.round((middle.length -1 /2))
+    }return word
+    }
+
+    function getNextLetter(letter){
+      let alphabet ='abcdefghijklmnopqrstuvwxzy'
+
+      for( let i = 0; i<letter.length; i ++){
+        if (letter[i].length === alphabet){
+        return
+        }
+      }return letter
+    }
+
+
+    function getAverage(average){
+
+      let x=[]
+
+      for(let i=0; i<average.length; i ++){
+        if (x[i] )
+      }
+    }
 
 
 
